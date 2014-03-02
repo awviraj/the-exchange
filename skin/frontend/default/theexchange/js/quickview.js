3b15dbc3810bac5c519db9768129785087784a75
admaduranga <web.dilhan@gmail.com>
2014-03-02 22:55:12 +0530

    Quick view

---
+++ skin/frontend/default/theexchange/js/quickview.js
@@ -0,0 +1,25 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Dilhan
+ * Date: 3/2/14
+ * Time: 10:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+jQuery(document).ready(function () {
+//    jQuery('.products-grid li').mouseenter(function (){
+//        jQuery(this).addClass('category-list-hover');
+//    });
+
+    jQuery('.products-grid li').hover(function (){
+        jQuery(this).addClass('active');
+    },
+    function (){
+        jQuery(this).removeClass('active');
+    }
+    );
+
+//    jQuery('.products-grid li').mouseout(function (){
+//        jQuery(this).removeClass('category-list-hover');
+//    });
+
+});
